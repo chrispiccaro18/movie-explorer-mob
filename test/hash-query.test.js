53b0938aad5b19c7692da7b45df8301f6fd20d6e
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-05 13:28:04 -0800

    created writePageToQuery test passing and looking guuuuud

--- test/hash-query.test.js
+++ test/hash-query.test.js
@@ -9,7 +9,7 @@ function writeSearchToQuery(existingQuery, searchTerm) {
     return searchParams.toString();
 }
 
-test('adds search to empty query', assert => {
+test('set search to query', assert => {
     // arrange
     const existingQuery = '';
     const searchTerm = 'star wars';
@@ -19,3 +19,28 @@ test('adds search to empty query', assert => {
     assert.equal(query, 'searchTerm=star+wars&page=1');
 });
 
+test('adds search to existing query', assert => {
+    // arrange
+    const existingQuery = 'searchTerm=star+wars&page=3';
+    const searchTerm = 'harry potter';
+    // act
+    const query = writeSearchToQuery(existingQuery, searchTerm);
+    // assert
+    assert.equal(query, 'searchTerm=harry+potter&page=1');
+});
+
+function writePageToQuery(existingQuery, page) {
+    const searchParams = new URLSearchParams(existingQuery);
+    searchParams.set('page', page);
+    return searchParams.toString();
+}
+
+test('write page to existing query', assert => {
+    // arrange
+    const existingQuery = 'searchTerm=star+wars&page=1';
+    const page = 3;
+    // act
+    const query = writePageToQuery(existingQuery, page);
+    // assert
+    assert.equal(query, 'searchTerm=star+wars&page=3');
+}); 

1b68e64faf5d074e336ed218bf2ac781390538fe
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-05 16:15:31 -0800

    moved function to separate module

---
+++ src/make-search-movie-url.js
@@ -0,0 +1,12 @@
+const MOVIE_SEARCH_URL = 'https://api.themoviedb.org/3/search/movie';
+const API_KEY = 'a9c1c53b2d714000fd04fb94fe4ad651';
+
+export default function makeSearchMovieUrl(queryOptions) {
+    const url = new URL(MOVIE_SEARCH_URL);
+    url.searchParams.set('api_key', API_KEY);
+    url.searchParams.set('language', 'en-us');
+    url.searchParams.set('include_adult', false);
+    url.searchParams.set('query', queryOptions.searchTerm);
+    url.searchParams.set('page', queryOptions.page);
+    return url.toString();
+}
--- test/make-search-movie-url.test.js
+++ test/make-search-movie-url.test.js
@@ -1,19 +1,7 @@
+import makeSearchMovieUrl from '../src/make-search-movie-url.js';
 const test = QUnit.test;
 
 QUnit.module('Make URL for fetch');
-
-const MOVIE_SEARCH_URL = 'https://api.themoviedb.org/3/search/movie';
-const API_KEY = 'a9c1c53b2d714000fd04fb94fe4ad651';
-
-function makeSearchMovieUrl(queryOptions) {
-    const url = new URL(MOVIE_SEARCH_URL);
-    url.searchParams.set('api_key', API_KEY);
-    url.searchParams.set('language', 'en-us');
-    url.searchParams.set('include_adult', false);
-    url.searchParams.set('query', queryOptions.searchTerm);
-    url.searchParams.set('page', queryOptions.page);
-    return url.toString();
-}
 
 test('includes query and page', assert => {
     // arrange

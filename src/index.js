1bb52e3057eb4bef73ff63f64fcbe73a473d3fd4
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-05 16:17:14 -0800

    added fetch and fetched movies from api

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import loadMovieList from './list-component.js';
 import { updateSearchBar } from './search-component.js';
 import './paging-component.js';
 import { readFromQuery } from './hash-query.js';
-loadMovieList(movieArray);
+import makeSearchMovieUrl from './make-search-movie-url.js';
+// loadMovieList(movieArray);
 
 window.addEventListener('hashchange', () => {
     const existingQuery = window.location.hash.slice(1);
     const queryOptions = readFromQuery(existingQuery);
-    console.log(queryOptions);
     updateSearchBar(queryOptions.searchTerm);
+    const url = makeSearchMovieUrl(queryOptions);
+    fetch(url)
+        .then(response => response.json())
+        .then(result => result.results)
+        .then(movieList => {
+            loadMovieList(movieList);
+        });
 }); 
